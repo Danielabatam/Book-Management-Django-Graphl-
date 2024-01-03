8c4c28bdfb1f068ce8bd820c9ecb2f64fffaf8e4
Daniela BATAM <danielabatam@gmail.com>
2024-01-03 16:37:46 +0100

    [Feat]: Create class Book in books/model.py

---
+++ books/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
