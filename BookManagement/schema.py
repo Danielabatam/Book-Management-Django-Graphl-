ef55538b37c518e0a99cbec4e2dd247510c8f38b
Daniela BATAM <danielabatam@gmail.com>
2024-01-04 21:53:48 +0100

    [Feat]:  Mutation Create Book

--- BookManagement/schema.py
+++ BookManagement/schema.py
@@ -10,6 +10,20 @@ class BookType(DjangoObjectType):
         fields = ("id", "title", "description", "created_at", "updated_at")
 
 
+class CreateBookMutation(graphene.Mutation):
+    class Arguments:
+        # title = graphene.String()
+        title = graphene.String(required=True)
+        description = graphene.String()
+
+    book = graphene.Field(BookType)
+
+    def mutate(self, info, title, description):
+        book = Book(title=title, description=description)
+        book.save()
+        return CreateBookMutation(book=book)
+
+
 class Query(graphene.ObjectType):
     hello = graphene.String(default_value="Hello")
     books = graphene.List(BookType)
@@ -22,10 +36,15 @@ class Query(graphene.ObjectType):
         return Book.objects.all()
 
     def resolve_book(self, info, id):
-        return Book.objects.get(pk=id) # Pourquoi  (pk=id) et non (id=id)
+        return Book.objects.get(pk=id)  # Pourquoi  (pk=id) et non (id=id)
 
 
-schema = graphene.Schema(query=Query)
+class Mutation(graphene.ObjectType):
+    # create_book = graphene.Field(BookType, title=graphene.String(), description=graphene.String())
+    create_book = CreateBookMutation.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
 
 # ================= Graph QL Query ================= #
 
