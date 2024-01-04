ddf633d50b98b2d88f4ba40f0d9fa91cc0850a95
Daniela BATAM <danielabatam@gmail.com>
2024-01-04 19:40:35 +0100

    [Feat]:  Query Books

--- BookManagement/schema.py
+++ BookManagement/schema.py
@@ -1,11 +1,24 @@
 import graphene
+from graphene_django import DjangoObjectType
+
+from books.models import Book
+
+
+class BookType(DjangoObjectType):
+    class Meta:
+        model = Book
+        fields = ("id", "title", "description", "created_at", "updated_at")
 
 
 class Query(graphene.ObjectType):
     hello = graphene.String(default_value="Hello")
+    books = graphene.List(BookType)
 
-    def resolve_hello(self, info):
-        return "World"
+    # def resolve_hello(self, info):
+    #  return "World"
+
+    def resolve_books(self, info):
+        return Book.objects.all()
 
 
 schema = graphene.Schema(query=Query)
